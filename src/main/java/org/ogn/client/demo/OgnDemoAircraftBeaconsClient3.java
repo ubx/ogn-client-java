e04d72ffea55c7a068a2a93903d6a5cc94c8451d
wbuczak <wojciech.buczak@gmail.com>
2015-03-20 18:47:40 +0100

    new demo added

---
+++ src/main/java/org/ogn/client/demo/OgnDemoAircraftBeaconsClient3.java
@@ -0,0 +1,87 @@
+/**
+ * Copyright (c) 2014 OGN, All Rights Reserved.
+ */
+
+package org.ogn.client.demo;
+
+import static java.lang.System.out;
+
+import org.ogn.client.AircraftBeaconListener;
+import org.ogn.client.OgnClient;
+import org.ogn.client.OgnClientConstants;
+import org.ogn.client.OgnClientFactory;
+import org.ogn.client.OgnClientProperties;
+import org.ogn.commons.beacon.AircraftBeacon;
+import org.ogn.commons.beacon.AircraftDescriptor;
+import org.ogn.commons.beacon.descriptor.AircraftDescriptorProvider;
+import org.ogn.commons.db.FileDbDescriptorProvider;
+import org.ogn.commons.db.ogn.OgnDb;
+import org.ogn.commons.igc.IgcLogger;
+import org.ogn.commons.utils.JsonUtils;
+
+/**
+ * A small demo program demonstrating the usage of the ogn-client with aircraft descriptor providers.
+ * 
+ * @author wbuczak
+ */
+public class OgnDemoAircraftBeaconsClient3 {
+
+    static {
+        // ignore parsing receiver beacons, we are not interested in them in this demo and there is
+        // no point in wasting CPU on that
+        System.setProperty(OgnClientProperties.PROP_OGN_CLIENT_IGNORE_RECEIVER_BEACONS, "true");
+    }
+
+    static IgcLogger igcLogger = new IgcLogger();
+
+    // enable if you want to log to IGC files
+    static boolean logIGC = false;
+
+    static class AcListener implements AircraftBeaconListener {
+
+        @Override
+        public void onUpdate(AircraftBeacon beacon, AircraftDescriptor descriptor) {
+            out.println("*********************************************");
+
+            // print the beacon
+            out.println(JsonUtils.toJson(beacon));
+
+            // if the aircraft has been recognized print its descriptor too
+            if (descriptor.isKnown()) {
+                out.println(JsonUtils.toJson(descriptor));
+            }
+
+            if (logIGC) {
+
+                if (descriptor.isKnown())
+                    igcLogger.log(descriptor.getRegNumber(), beacon.getLat(), beacon.getLon(), beacon.getAlt(),
+                            beacon.getRawPacket());
+                else
+                    igcLogger.log(beacon.getId(), beacon.getLat(), beacon.getLon(), beacon.getAlt(),
+                            beacon.getRawPacket());
+            }// if
+
+            out.println("*********************************************");
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        AircraftDescriptorProvider adp = new FileDbDescriptorProvider<OgnDb>(OgnDb.class);
+
+        // create ogn client and pass it a reference to the previously created descriptor provider
+        OgnClient client = OgnClientFactory.createClient();
+
+        OgnClient client2 = OgnClientFactory.getBuilder().port(OgnClientConstants.OGN_DEFAULT_SRV_PORT + 1000)
+                .descriptorProviders(adp).build();
+
+        System.out.println("connecting...");
+        client.connect();
+        client2.connect();
+
+        client.subscribeToAircraftBeacons(new AcListener());
+        client2.subscribeToAircraftBeacons(new AcListener());
+
+        Thread.sleep(Long.MAX_VALUE);
+    }
+}
